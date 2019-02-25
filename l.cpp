3178e900f41e693b1759953d14cc04eb6c2672ed
Don <aleronego@gmail.com>
2019-02-25 17:40:21 +0200

    7\10

--- l.cpp
+++ l.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+//#include <vector>
 using namespace std;
 
 void print(int arr[100][100], int n)
@@ -18,12 +18,14 @@ void print(int arr[100][100], int n)
 
 int main(int argc, char const *argv[])
 {
+    const int MAX = 101;
     int dev,pro,pairs;
     cin >> dev >> pro >> pairs;
     int n = (dev>pro?dev:pro)+1;
-    int arr[100][100];
-    vector<pair<int,int> >res;
-
+    int arr[MAX][MAX];
+    //vector<pair<int,int> >res;
+    int res[MAX][2];
+    int resCurr=0;
     //int visited[100];
     //string res;
     for(int i = 0; i < pairs; ++i)
@@ -59,7 +61,11 @@ int main(int argc, char const *argv[])
         int choice = (single?single:max);
         if(choice!=0)
         {
-            res.push_back(make_pair(i, choice));
+            //res.push_back(make_pair(i, choice));
+            ++resCurr;  
+            res[resCurr][0] = i; 
+            res[resCurr][1] = choice;
+
             for(int k = 1; k <= dev; ++k)
             {
                 if(arr[k][choice])
@@ -75,9 +81,13 @@ int main(int argc, char const *argv[])
     //cout << "\nAfter";
     //print(arr, n);
 
-    cout << res.size() << endl;
-    for(vector<pair<int,int> >::iterator it = res.begin(); it != res.end(); ++it)
-        cout << it->first << " " << it->second << endl;
+    // cout << res.size() << endl;
+    // for(vector<pair<int,int> >::iterator it = res.begin(); it != res.end(); ++it)
+    //     cout << it->first << " " << it->second << endl;
 
+    cout << resCurr << endl;
+    if(resCurr)
+        for(int i = 1; i <= resCurr; ++i)
+            cout << res[i][0] << " " << res[i][1] << endl;
     return 0;
 }
